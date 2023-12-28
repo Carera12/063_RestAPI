cbb5254480f07ad617361cc13a1942a662ffeacc
Carera12 <silvicarera123@gmail.com>
2023-12-28 20:52:38 +0700

    add Row di text&icon alamat

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/HomeScreen.kt
@@ -13,11 +13,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -98,6 +100,7 @@ fun KontakLayout(
 @Composable
 fun KontakCard(
     kontak: Kontak,
+    onDeleteClick: (Kontak) -> Unit = {},
     modifier: Modifier= Modifier
 ) {
     Card(
@@ -126,10 +129,21 @@ fun KontakCard(
                     style = MaterialTheme.typography.titleMedium
                 )
             }
-            Text(
-                text = kontak.alamat,
-                style = MaterialTheme.typography.titleMedium
-            )
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = kontak.alamat,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Spacer(Modifier.weight(1f))
+                IconButton(onClick = { onDeleteClick(kontak) }) {
+                    Icon(
+                        imageVector = Icons.Default.Delete,
+                        contentDescription = null)
+                }
+            }
         }
     }
 }
